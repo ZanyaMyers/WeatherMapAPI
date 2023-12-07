19820af53c8f1732f4bfa2db333e0ebc4e644d4f
ZanyaMyers <myerszanya@gmail.com>
2023-12-07 09:32:24 -0800

    London temp

--- WeatherMapAPI/Program.cs
+++ WeatherMapAPI/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace WeatherMapAPI
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherMapAPI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+
+            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=bff200da1d31b961f942cefd6c219f8d&units=imperial";
+            var response = client.GetStringAsync(weatherURL).Result;
+
+            JObject weatherObject = JObject.Parse(response);
+
+            Console.WriteLine("The following is the weather information for London");
+            Console.WriteLine($"The temperature is {weatherObject["main"]["temp"]}"); 
+           
+
+
         }
     }
 }
